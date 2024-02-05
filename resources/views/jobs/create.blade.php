dab78d844e1c8de63fcc4389fb2db5b854b50465
Liam <liamjwatson@aol.com>
2024-02-05 16:06:27 +0000

    added old helper function to form to keep the old data in form, when error message shows

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -22,6 +22,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="company"
+                    value="{{old('company')}}"
                 />
                 @error('company')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -37,6 +38,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="title"
                     placeholder="Example: Senior Laravel Developer"
+                    value="{{old('title')}}"
                 />
 
                 @error('title')
@@ -55,6 +57,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="location"
                     placeholder="Example: Remote, Boston MA, etc"
+                    value="{{old('location')}}"
                 />
                 @error('location')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -69,6 +72,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="email"
+                    value="{{old('email')}}"
                 />
                 @error('email')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -86,6 +90,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="website"
+                    value="{{old('website')}}"
                 />
                 @error('website')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -101,6 +106,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="tags"
                     placeholder="Example: Laravel, Backend, Postgres, etc"
+                    value="{{old('tags')}}"
                 />
                 @error('tags')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -130,7 +136,7 @@
                     name="description"
                     rows="10"
                     placeholder="Include tasks, requirements, salary, etc"
-                ></textarea>
+                >{{old('description')}}</textarea>
                 @error('description')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
