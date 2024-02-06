cfcb0a556a8a62e24048a4404d2fb799698abae1
Liam <liamjwatson@aol.com>
2024-02-06 14:24:12 +0000

    delete form added to show.blade template, which goes to /job/{{-

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -57,6 +57,14 @@
         {{-- font awesome is coming from layout --}}
              <i class="fa-solid fa-pencil"></i> Edit
         </a>
+
+        <form method="POST" action="/job/{{$job->id}}">
+        @csrf
+        @method('DELETE')
+        <button class="text-red-500">
+            <i class="fa-solid fa-trash"></i>Delete
+        </button>
+        </form>
     </x-card>
 </div>
 
