06f683dae3a726e19ddf813993d74f5fd0c1b0e3
Liam <liamjwatson@aol.com>
2024-02-07 00:18:33 +0000

    manage job form connected- for each job belonging to the logged in user, a table row will be displayed with the job title, edit and delete links. If there are no jobs, one table row will be displayed saying 'no jobs found'

--- resources/views/jobs/manage.blade.php
+++ resources/views/jobs/manage.blade.php
@@ -1,5 +1,4 @@
 <x-layout>
-
     <x-card class='p-10'>
         <header>
             <h1
@@ -11,73 +10,53 @@
 
         <table class="w-full table-auto rounded-sm">
             <tbody>
-                <tr class="border-gray-300">
-                    <td
-                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
-                    >
-                        <a href="show.html">
-                            Laravel Senior Developer
-                        </a>
-                    </td>
-                    <td
-                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
-                    >
-                        <a
-                            href="edit.html"
-                            class="text-blue-400 px-6 py-2 rounded-xl"
-                            ><i
-                                class="fa-solid fa-pen-to-square"
-                            ></i>
-                            Edit</a
-                        >
-                    </td>
-                    <td
-                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
-                    >
-                        <form action="">
-                            <button class="text-red-600">
-                                <i
-                                    class="fa-solid fa-trash-can"
-                                ></i>
-                                Delete
-                            </button>
-                        </form>
-                    </td>
-                </tr>
+                {{-- if jobs is not empty --}}
+                @unless ($jobs->isEmpty())
+                    {{-- create a tr for each job --}}
+                    @foreach($jobs as $job)
+                        <tr class="border-gray-300">
+                            <td
+                            class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                            >
+                            <a href="/jobs/{{$job->id}}">
+                                    {{$job->title}}
+                                </a>
+                            </td>
+                            <td
+                                class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                            >
+                                <a
+                                    href="/jobs/{{$job->id}}/edit"
+                                    class="text-blue-400 px-6 py-2 rounded-xl"
+                                    ><i
+                                        class="fa-solid fa-pen-to-square"
+                                    ></i>
+                                    Edit</a
+                                >
+                            </td>
+                            <td
+                                class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                            >
+                            <form method="POST" action="/job/{{$job->id}}">
+                                @csrf
+                                @method('DELETE')
+                                <button class="text-red-500">
+                                    <i class="fa-solid fa-trash"></i>Delete
+                                </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                {{--if jobs is empty make a tr with 'NO JOBS FOUND'  --}}
+                @else
+                    <tr class="border-gray-300">
+                        <td class="px-4 py-8 border-t-b border-gray-300 text-lg">
+                            <p class="text-center">No Jobs Found</p>
+                        </td>
+                    </tr>
+                @endunless
 
-                <tr class="border-gray-300">
-                    <td
-                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
-                    >
-                        <a href="show.html">
-                            Junior Developer Opening
-                        </a>
-                    </td>
-                    <td
-                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
-                    >
-                        <a
-                            href="edit.html"
-                            class="text-blue-400 px-6 py-2 rounded-xl"
-                            ><i
-                                class="fa-solid fa-pen-to-square"
-                            ></i>
-                            Edit</a
-                        >
-                    </td>
-                    <td
-                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
-                    >
-                        <form action="">
-                            <button class="text-red-600">
-                                <i
-                                    class="fa-solid fa-trash-can"
-                                ></i>
-                                Delete
-                            </button>
-                        </form>
-                    </td>
-                </tr>
+               
             </tbody>
         </table>
     </x-card>
